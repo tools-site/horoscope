d29d5caa75a9ed8e31b00153df806c4d7e2da88e
RyuSeiri <56245152+RyuSeiri@users.noreply.github.com>
2024-03-04 21:20:52 +0900

    update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const signDateRanges = {
-  Aries: "3月21日 - 4月19日",
-  Taurus: "4月20日 - 5月20日",
-  Gemini: "5月21日 - 6月20日",
-  Cancer: "6月21日 - 7月22日",
-  Leo: "7月23日 - 8月22日",
-  Virgo: "8月23日 - 9月22日",
-  Libra: "9月23日 - 10月22日",
-  Scorpio: "10月23日 - 11月21日",
-  Sagittarius: "11月22日 - 12月21日",
-  Capricorn: "12月22日 - 1月19日",
-  Aquarius: "1月20日 - 2月18日",
-  Pisces: "2月19日 - 3月20日",
+  "白羊座": "3月21日 - 4月19日",
+  "金牛座": "4月20日 - 5月20日",
+  "双子座": "5月21日 - 6月20日",
+  "巨蟹座": "6月21日 - 7月22日",
+  "狮子座": "7月23日 - 8月22日",
+  "处女座": "8月23日 - 9月22日",
+  "天秤座": "9月23日 - 10月22日",
+  "天蝎座": "10月23日 - 11月21日",
+  "射手座": "11月22日 - 12月21日",
+  "摩羯座": "12月22日 - 1月19日",
+  "水瓶座": "1月20日 - 2月18日",
+  "双鱼座": "2月19日 - 3月20日",
 };
 
 async function getHoroscope() {
@@ -25,7 +25,7 @@ async function getHoroscope() {
     const sinData = todayDatas[sign]
     horoscopeResult.textContent = `今日${sign}的运势：${sinData.summary}`;
     dateRange.textContent = `星座日期范围：${signDateRanges[sign]}`;
-  } catch (e) {
+  } catch (error) {
     console.error("获取运势失败:", error);
     horoscopeResult.textContent = "获取运势失败，请稍后再试。";
   }
